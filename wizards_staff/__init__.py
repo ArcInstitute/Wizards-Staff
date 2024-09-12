aebca7d0335669c9664f9ebf5b039bca93db44cf
mrjholt <mr.jesseholt@gmail.com>
2024-09-12 21:19:32 +0000

    Refactor: Ensure all essential functions are included in __init__.py
    - Added missing functions from `pwc.py` and `plotting.py` to `__init__.py`.
    - Ensured all relevant utilities are accessible across the package.

--- wizards_staff/__init__.py
+++ wizards_staff/__init__.py
@@ -4,7 +4,7 @@
 from .king_gizzard import run_all
 
 # From pwc.py
-from .pwc import run_pwc, calc_pwc_mn
+from .pwc import run_pwc, calc_pwc_mn, extract_intra_inter_nsp_neurons, gen_mn_std_means, gen_polynomial_fit
 
 # From metadata.py
 from .metadata import load_and_process_metadata, append_metadata_to_dfs
@@ -13,17 +13,16 @@ from .metadata import load_and_process_metadata, append_metadata_to_dfs
 from .metrics import calc_rise_tm, calc_fwhm_spikes, calc_frpm, convert_f_to_cs, calc_mask_shape_metrics
 
 # From plotting.py
-from .plotting import plot_spatial_activity_map, plot_kmeans_heatmap, plot_cluster_activity
+from .plotting import plot_spatial_activity_map, plot_kmeans_heatmap, plot_cluster_activity, overlay_images, plot_montage
 
 # From utils.py
 from .utils import categorize_files, spatial_filtering, load_required_files, load_and_filter_files
 
-# Define what is accessible when doing `from wizards_staff import *`
 __all__ = [
     "run_all",
-    "run_pwc", "calc_pwc_mn",
+    "run_pwc", "calc_pwc_mn", "extract_intra_inter_nsp_neurons", "gen_mn_std_means", "gen_polynomial_fit",
     "load_and_process_metadata", "append_metadata_to_dfs",
     "calc_rise_tm", "calc_fwhm_spikes", "calc_frpm", "convert_f_to_cs", "calc_mask_shape_metrics",
-    "plot_spatial_activity_map", "plot_kmeans_heatmap", "plot_cluster_activity", "spatial_filtering",
-    "categorize_files"
+    "plot_spatial_activity_map", "plot_kmeans_heatmap", "plot_cluster_activity", "overlay_images", "plot_montage",
+    "categorize_files", "spatial_filtering", "load_required_files", "load_and_filter_files"
 ]
