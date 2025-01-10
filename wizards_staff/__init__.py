a248dd65d462354325be07f4ccd38f0a0bd087c1
nick-youngblut <nyoungb2@gmail.com>
2025-01-10 11:23:20 -0800

    Disable multithreading for NumPy to  prevent stalling during deconvolution.constrained_foopsi

--- wizards_staff/__init__.py
+++ wizards_staff/__init__.py
@@ -1,3 +1,9 @@
-from .wizards.orb import Orb
+# disable multithreading for numpy
+import os
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
 
+# import the Orb class
+from .wizards.orb import Orb
 __all__ = ['Orb']
